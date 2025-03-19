81abd26f42bc34d56d420c9802bcc8fdcf1692d8
Michael Currie <mcurrie@fling.asia>
2025-03-19 17:13:27 +0700

    Update __init__.py

--- aruco_estimator/__init__.py
+++ aruco_estimator/__init__.py
@@ -15,7 +15,7 @@ See LICENSE file for more information.
 from . import aruco  # noqa: F401
 from . import aruco_scale_factor  # noqa: F401
 from . import base  # noqa: F401
+from . import download  # noqa: F401
 from . import opt  # noqa: F401
 from . import utils  # noqa: F401
 from . import visualization  # noqa: F401
-from .download_dataset import download  # noqa: F401
