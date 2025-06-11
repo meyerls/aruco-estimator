593cbb62c33ba8772ff26da0a4af9d4f5001d95e
Nile Walker <nile.walker@jhuapl.edu>
2025-06-11 16:45:53 -0400

    Updated colmap_wrapper version

--- aruco_estimator/localizers/aruco_localizer.py
+++ aruco_estimator/localizers/aruco_localizer.py
@@ -5,6 +5,7 @@ Copyright (c) 2022 Lukas Meyer
 Licensed under the MIT License.
 See LICENSE file for more information.
 """
+
 import logging
 import os
 import time
@@ -16,13 +17,13 @@ from typing import Tuple, Union
 import cv2
 import numpy as np
 import open3d as o3d
-from colmap_wrapper.colmap import COLMAP, COLMAPProject
-from colmap_wrapper.colmap.bin import (
+from colmap_wrapper.dataloader import COLMAPProject
+from colmap_wrapper.dataloader.bin import (
     write_cameras_text,
     write_images_text,
     write_points3D_text,
 )
-from colmap_wrapper.colmap.utils import generate_colmap_sparse_pc
+from colmap_wrapper.dataloader.utils import generate_colmap_sparse_pc
 from tqdm import tqdm
 
 from ..aruco import detect_aruco_marker, ray_cast_aruco_corners
@@ -42,15 +43,23 @@ def timeit(func):
         result = func(*args, **kwargs)
         end_time = time.perf_counter()
         total_time = end_time - start_time
-        logging.debug(f'Function {func.__name__}{args} {kwargs} took {total_time:.4f} seconds')
+        logging.debug(
+            f"Function {func.__name__}{args} {kwargs} took {total_time:.4f} seconds"
+        )
         return result
 
     return timeit_wrapper
 
 
 class ArucoLocalizer(LocalizerBase):
-    def __init__(self, photogrammetry_software: Union[COLMAPProject, COLMAP], aruco_size: float,
-                 dense_path: str = 'fused.ply', dict_type: int = cv2.aruco.DICT_4X4_50, target_id: int = 0):
+    def __init__(
+        self,
+        photogrammetry_software: COLMAPProject,
+        aruco_size: float,
+        dense_path: str = "fused.ply",
+        dict_type: int = cv2.aruco.DICT_4X4_50,
+        target_id: int = 0,
+    ):
         """
         This class is used to determine 3D points of the aruco marker, which are used to compute a scaling factor.
         In the following the workflow is shortly described.
@@ -95,7 +104,7 @@ class ArucoLocalizer(LocalizerBase):
         self.aruco_corners_3d = None
         self.aruco_dict_type = dict_type
         self.target_id = target_id
-        
+
         # Store all detected ArUco markers
         self.all_aruco_ids = []
         self.all_aruco_corners_3d = {}  # Dictionary mapping ArUco IDs to their 3D corner positions
@@ -108,13 +117,16 @@ class ArucoLocalizer(LocalizerBase):
         # Multi Processing
         self.progress_bar = True
         self.num_processes = 12 if os.cpu_count() > 12 else os.cpu_count()
-        logging.debug('Num process: %s', self.num_processes)
+        logging.debug("Num process: %s", self.num_processes)
         self.image_names = []
         # Prepare parsed data for multi processing
         for image_idx in self.photogrammetry_software.images.keys():
-            self.image_names.append(self.photogrammetry_software._src_image_path.joinpath(
-                self.photogrammetry_software.images[image_idx].name).__str__())
-   
+            self.image_names.append(
+                self.photogrammetry_software._src_image_path.joinpath(
+                    self.photogrammetry_software.images[image_idx].name
+                ).__str__()
+            )
+
         self.aruco_size = aruco_size
 
     def run(self) -> [np.ndarray, None]:
@@ -125,19 +137,23 @@ class ArucoLocalizer(LocalizerBase):
         """
         self.__detect()
         self.__ray_cast()
-        
+
         # Check if target ID was found
         if self.target_id not in self.all_aruco_ids:
-            raise ValueError(f"Target ArUco ID {self.target_id} not found in images. Available IDs: {self.all_aruco_ids}")
-        
+            raise ValueError(
+                f"Target ArUco ID {self.target_id} not found in images. Available IDs: {self.all_aruco_ids}"
+            )
+
         # Calculate 3D corners for target ID
-        self.aruco_corners_3d = intersect_parallelized(P0=self.P0.reshape(len(self.P0) // 3, 3),
-                                                       N=self.N.reshape(len(self.N) // 12, 4, 3))
+        self.aruco_corners_3d = intersect_parallelized(
+            P0=self.P0.reshape(len(self.P0) // 3, 3),
+            N=self.N.reshape(len(self.N) // 12, 4, 3),
+        )
         self.aruco_distance = self.__evaluate(self.aruco_corners_3d)
-        
+
         # Store the target corners in the dictionary
         self.all_aruco_corners_3d[self.target_id] = self.aruco_corners_3d
-        
+
         # Calculate positions for all detected ArUco markers
         self.calculate_all_aruco_positions()
 
@@ -153,45 +169,75 @@ class ArucoLocalizer(LocalizerBase):
         :return:
         """
         with Pool(self.num_processes) as p:
-            result = list(tqdm(p.imap(
-                partial(detect_aruco_marker, dict_type=self.aruco_dict_type),
-                self.image_names), total=len(self.image_names), disable=not self.progress_bar))
-        
+            result = list(
+                tqdm(
+                    p.imap(
+                        partial(detect_aruco_marker, dict_type=self.aruco_dict_type),
+                        self.image_names,
+                    ),
+                    total=len(self.image_names),
+                    disable=not self.progress_bar,
+                )
+            )
+
         if len(result) != len(self.photogrammetry_software.images):
-            raise ValueError("Thread return has not the same length as the input parameters!")
+            raise ValueError(
+                "Thread return has not the same length as the input parameters!"
+            )
 
         aruco_ids = []
         self.all_aruco_ids = []  # Reset the list of all ArUco IDs
 
-        for image_idx, image_key in enumerate(self.photogrammetry_software.images.keys()):
-            ratio_x = self.photogrammetry_software.cameras[1].width / result[image_idx][2][1]
-            ratio_y = self.photogrammetry_software.cameras[1].height / result[image_idx][2][0]
+        for image_idx, image_key in enumerate(
+            self.photogrammetry_software.images.keys()
+        ):
+            ratio_x = (
+                self.photogrammetry_software.cameras[1].width / result[image_idx][2][1]
+            )
+            ratio_y = (
+                self.photogrammetry_software.cameras[1].height / result[image_idx][2][0]
+            )
             if result[image_idx][0] != None:
-                corners = (np.expand_dims(np.vstack([result[image_idx][0][0][0, :, 0] * ratio_y,
-                                                     result[image_idx][0][0][0, :, 1] * ratio_x]).T, axis=0),)
+                corners = (
+                    np.expand_dims(
+                        np.vstack(
+                            [
+                                result[image_idx][0][0][0, :, 0] * ratio_y,
+                                result[image_idx][0][0][0, :, 1] * ratio_x,
+                            ]
+                        ).T,
+                        axis=0,
+                    ),
+                )
                 self.photogrammetry_software.images[image_key].aruco_corners = corners
-                
+
                 # Store the ArUco ID
                 current_id = result[image_idx][1][0][0]
                 aruco_ids.append(current_id)
-                
+
                 # Add to the list of all ArUco IDs if not already present
                 if current_id not in self.all_aruco_ids:
                     self.all_aruco_ids.append(current_id)
             else:
-                self.photogrammetry_software.images[image_key].aruco_corners = result[image_idx][0]
+                self.photogrammetry_software.images[image_key].aruco_corners = result[
+                    image_idx
+                ][0]
 
-            self.photogrammetry_software.images[image_key].aruco_id = result[image_idx][1]
-            self.photogrammetry_software.images[image_key].image_path = self.image_names[image_idx]
+            self.photogrammetry_software.images[image_key].aruco_id = result[image_idx][
+                1
+            ]
+            self.photogrammetry_software.images[
+                image_key
+            ].image_path = self.image_names[image_idx]
 
         # Determine the dominant ArUco ID (most frequent)
         if aruco_ids:
             self.dominant_aruco_id = np.argmax(np.bincount(aruco_ids))
-            
+
             # If target_id is not specified (or is -1), use the dominant ID
             if self.target_id == -1:
                 self.target_id = self.dominant_aruco_id
-                
+
             logging.info(f"Detected ArUco IDs: {sorted(self.all_aruco_ids)}")
             logging.info(f"Dominant ArUco ID: {self.dominant_aruco_id}")
             logging.info(f"Target ArUco ID: {self.target_id}")
@@ -211,27 +257,37 @@ class ArucoLocalizer(LocalizerBase):
         # Reset P0 and N for the target ID
         self.P0 = np.array([])
         self.N = np.array([])
-        
+
         # Dictionary to store P0 and N for each ArUco ID
         self.all_P0 = {id: np.array([]) for id in self.all_aruco_ids}
         self.all_N = {id: np.array([]) for id in self.all_aruco_ids}
-        
+
         for image_idx in self.photogrammetry_software.images.keys():
             if self.photogrammetry_software.images[image_idx].aruco_corners is not None:
-                current_id = self.photogrammetry_software.images[image_idx].aruco_id[0, 0]
-                
+                current_id = self.photogrammetry_software.images[image_idx].aruco_id[
+                    0, 0
+                ]
+
                 # Process all detected ArUco markers
-                p0, n = ray_cast_aruco_corners(extrinsics=self.photogrammetry_software.images[image_idx].extrinsics,
-                                               intrinsics=self.photogrammetry_software.images[image_idx].intrinsics.K,
-                                               corners=self.photogrammetry_software.images[image_idx].aruco_corners)
-                
+                p0, n = ray_cast_aruco_corners(
+                    extrinsics=self.photogrammetry_software.images[
+                        image_idx
+                    ].extrinsics,
+                    intrinsics=self.photogrammetry_software.images[
+                        image_idx
+                    ].intrinsics.K,
+                    corners=self.photogrammetry_software.images[
+                        image_idx
+                    ].aruco_corners,
+                )
+
                 self.photogrammetry_software.images[image_idx].p0 = p0
                 self.photogrammetry_software.images[image_idx].n = n
-                
+
                 # Store data for all ArUco IDs
                 self.all_P0[current_id] = np.append(self.all_P0[current_id], p0)
                 self.all_N[current_id] = np.append(self.all_N[current_id], n)
-                
+
                 # If this is the target ID, also store in the main P0 and N arrays
                 if current_id == self.target_id:
                     self.P0 = np.append(self.P0, p0)
@@ -271,9 +327,9 @@ class ArucoLocalizer(LocalizerBase):
             sf[i - 1] = (self.aruco_size) / aruco_dist
 
         plt.figure()
-        plt.title('Scale Factor Estimation over Number of Images')
-        plt.xlabel('# Images')
-        plt.ylabel('Scale Factor')
+        plt.title("Scale Factor Estimation over Number of Images")
+        plt.xlabel("# Images")
+        plt.ylabel("Scale Factor")
         plt.grid()
         plt.plot(np.linspace(1, len(self.P0) // 3, len(self.P0) // 3)[1:], sf[1:])
         plt.show()
@@ -295,45 +351,60 @@ class ArucoLocalizer(LocalizerBase):
         """
 
         self.scale_factor = (self.aruco_size) / self.aruco_distance
-        self.photogrammetry_software.dense_scaled = deepcopy(self.photogrammetry_software.dense)
-        self.photogrammetry_software.dense_scaled.scale(scale=self.scale_factor, center=np.asarray([0., 0., 0.]))
+        self.photogrammetry_software.dense_scaled = deepcopy(
+            self.photogrammetry_software.dense
+        )
+        self.photogrammetry_software.dense_scaled.scale(
+            scale=self.scale_factor, center=np.asarray([0.0, 0.0, 0.0])
+        )
 
         self.sparse_scaled = deepcopy(self.photogrammetry_software.sparse)
         for num in self.photogrammetry_software.sparse.keys():
-            self.sparse_scaled[num].xyz = self.sparse_scaled[num].xyz * self.scale_factor
+            self.sparse_scaled[num].xyz = (
+                self.sparse_scaled[num].xyz * self.scale_factor
+            )
 
         # ToDo: Scale tvec and save
-        self.photogrammetry_software.images_scaled = deepcopy(self.photogrammetry_software.images)
+        self.photogrammetry_software.images_scaled = deepcopy(
+            self.photogrammetry_software.images
+        )
         for idx in self.photogrammetry_software.images_scaled.keys():
-            self.photogrammetry_software.images_scaled[idx].tvec = self.photogrammetry_software.images[
-                                                                       idx].tvec * self.scale_factor
+            self.photogrammetry_software.images_scaled[idx].tvec = (
+                self.photogrammetry_software.images[idx].tvec * self.scale_factor
+            )
 
         return self.photogrammetry_software.dense_scaled, self.scale_factor
 
     def calculate_all_aruco_positions(self):
         """
         Calculate 3D positions for all detected ArUco markers.
-        
+
         :return: Dictionary mapping ArUco IDs to their 3D corner positions
         """
         # Calculate 3D corners for all ArUco IDs
         for aruco_id in self.all_aruco_ids:
-            if len(self.all_P0[aruco_id]) > 0:  # Only process if we have data for this ID
-                P0_reshaped = self.all_P0[aruco_id].reshape(len(self.all_P0[aruco_id]) // 3, 3)
-                N_reshaped = self.all_N[aruco_id].reshape(len(self.all_N[aruco_id]) // 12, 4, 3)
-                
+            if (
+                len(self.all_P0[aruco_id]) > 0
+            ):  # Only process if we have data for this ID
+                P0_reshaped = self.all_P0[aruco_id].reshape(
+                    len(self.all_P0[aruco_id]) // 3, 3
+                )
+                N_reshaped = self.all_N[aruco_id].reshape(
+                    len(self.all_N[aruco_id]) // 12, 4, 3
+                )
+
                 if len(P0_reshaped) > 0 and len(N_reshaped) > 0:
                     corners_3d = intersect_parallelized(P0=P0_reshaped, N=N_reshaped)
                     self.all_aruco_corners_3d[aruco_id] = corners_3d
                     logging.info(f"Calculated 3D corners for ArUco ID {aruco_id}")
-        
+
         return self.all_aruco_corners_3d
-    
+
     def get_all_aruco_positions(self):
         """
         Get the 3D positions of all detected ArUco markers.
         If positions haven't been calculated yet, calculate them.
-        
+
         :return: Dictionary mapping ArUco IDs to their 3D corner positions
         """
         if not self.all_aruco_corners_3d:
@@ -345,10 +416,12 @@ class ArucoLocalizer(LocalizerBase):
         Write scaled data to files.
         """
         pcd_scaled = self.photogrammetry_software._project_path
-        sparse_scaled_path = self.photogrammetry_software._project_path.joinpath('sparse_scaled')
-        cameras_scaled = sparse_scaled_path.joinpath('cameras.txt')
-        images_scaled = sparse_scaled_path.joinpath('images.txt')
-        points_scaled = sparse_scaled_path.joinpath('points3D.txt')
+        sparse_scaled_path = self.photogrammetry_software._project_path.joinpath(
+            "sparse_scaled"
+        )
+        cameras_scaled = sparse_scaled_path.joinpath("cameras.txt")
+        images_scaled = sparse_scaled_path.joinpath("images.txt")
+        points_scaled = sparse_scaled_path.joinpath("points3D.txt")
 
         sparse_scaled_path.mkdir(parents=True, exist_ok=True)
 
@@ -356,30 +429,35 @@ class ArucoLocalizer(LocalizerBase):
         write_images_text(self.photogrammetry_software.images_scaled, images_scaled)
         write_points3D_text(self.sparse_scaled, points_scaled)
 
-        o3d.io.write_point_cloud(os.path.join(pcd_scaled, 'scaled.ply'), self.photogrammetry_software.dense_scaled)
+        o3d.io.write_point_cloud(
+            os.path.join(pcd_scaled, "scaled.ply"),
+            self.photogrammetry_software.dense_scaled,
+        )
 
         # Save scale factor
-        scale_factor_file_name = self.photogrammetry_software._project_path.joinpath('sparse_scaled/scale_factor.txt')
+        scale_factor_file_name = self.photogrammetry_software._project_path.joinpath(
+            "sparse_scaled/scale_factor.txt"
+        )
         np.savetxt(scale_factor_file_name, np.array([self.scale_factor]))
 
 
-if __name__ == '__main__':
-    from colmap_wrapper.colmap import COLMAP
-
-    from aruco_estimator.visualization import ArucoVisualization
-
-    project = COLMAP(project_path='../data/door', image_resize=0.4)
-
-    aruco_localizer = ArucoLocalizer(photogrammetry_software=project, aruco_size=0.15)
-    aruco_distance, aruco_points3d = aruco_localizer.run()
-    logging.info('Mean distance between aruco markers: ', aruco_distance)
-
-    aruco_localizer.analyze()
-
-    dense, scale_factor = aruco_localizer.apply()
-    logging.info('Point cloud and poses are scaled by: ', scale_factor)
-
-    vis = ArucoVisualization(aruco_colmap=aruco_localizer)
-    vis.visualization(frustum_scale=0.7, point_size=0.1)
-
-    aruco_localizer.write_data()
+# if __name__ == '__main__':
+#     from colmap_wrapper.colmap import COLMAP
+#
+#     from aruco_estimator.visualization import ArucoVisualization
+#
+#     project = COLMAP(project_path='../data/door', image_resize=0.4)
+#
+#     aruco_localizer = ArucoLocalizer(photogrammetry_software=project, aruco_size=0.15)
+#     aruco_distance, aruco_points3d = aruco_localizer.run()
+#     logging.info('Mean distance between aruco markers: ', aruco_distance)
+#
+#     aruco_localizer.analyze()
+#
+#     dense, scale_factor = aruco_localizer.apply()
+#     logging.info('Point cloud and poses are scaled by: ', scale_factor)
+#
+#     vis = ArucoVisualization(aruco_colmap=aruco_localizer)
+#     vis.visualization(frustum_scale=0.7, point_size=0.1)
+#
+#     aruco_localizer.write_data()
